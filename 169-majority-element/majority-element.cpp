fa83349f4b36644b4dd86ee60c6e0915878afd55
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-19 12:27:54 +0530

    Update majority-element.cpp

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,10 +4,13 @@ public:
         int n = nums.size();
         int freq = 0, ans = -1;
         for(int i=0; i<n; i++){
-            if(freq == 0) ans = nums[i];
-            if(ans == nums[i]) freq++;
-            else freq--;
+            if(freq == 0) ans = nums[i];    //If count is 0, set candidate to the current element
+            if(ans == nums[i]) freq++;      //If the current element matches the candidate, increment count.
+            else freq--;                    //If it differs, decrement count
         }
         return ans;
     }
 };
+
+// Moore's Voting Algorithm is an efficient O(N) time and O(1) space algorithm used to find the majority element in an array—an element that appears more than [N/2] times. 
+// It works by maintaining a candidate and a counter, cancelling out distinct elements until the majority candidate remains
